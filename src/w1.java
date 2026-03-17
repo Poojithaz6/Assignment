79eb62ca0a147dc88e0aea42a65392d1ff17031b
poojithaz6 <poojithav183@gmail.com>
2026-03-17 10:45:22 +0530

    problem 2

--- src/w1.java
+++ src/w1.java
@@ -1,43 +1,36 @@
 import java.util.*;
 
 public class w1 {
-    static class UsernameChecker {
-        Map<String, Integer> users = new HashMap<>();
-        Map<String, Integer> freq = new HashMap<>();
+    static class InventoryManager {
+        Map<String, Integer> stock = new HashMap<>();
+        Map<String, Queue<Integer>> wait = new HashMap<>();
 
-        boolean check(String name) {
-            freq.put(name, freq.getOrDefault(name, 0) + 1);
-            return !users.containsKey(name);
+        void addProduct(String id, int count) {
+            stock.put(id, count);
+            wait.put(id, new LinkedList<>());
         }
 
-        List<String> suggest(String name) {
-            List<String> res = new ArrayList<>();
-            for (int i = 1; i <= 3; i++) res.add(name + i);
-            res.add(name.replace("_", "."));
-            return res;
-        }
-
-        void add(String name, int id) {
-            users.put(name, id);
-        }
-
-        String mostAttempted() {
-            String ans = "";
-            int max = 0;
-            for (String k : freq.keySet()) {
-                if (freq.get(k) > max) {
-                    max = freq.get(k);
-                    ans = k;
-                }
+        synchronized String purchase(String id, int user) {
+            int s = stock.getOrDefault(id, 0);
+            if (s > 0) {
+                stock.put(id, s - 1);
+                return "Success";
+            } else {
+                wait.get(id).offer(user);
+                return "Waitlist";
             }
-            return ans;
+        }
+
+        int check(String id) {
+            return stock.getOrDefault(id, 0);
         }
     }
 
     public static void main(String[] args) {
-        UsernameChecker u = new UsernameChecker();
-        u.add("john_doe", 1);
-        System.out.println(u.check("john_doe"));
-        System.out.println(u.suggest("john_doe"));
+        InventoryManager i = new InventoryManager();
+        i.addProduct("p1", 2);
+        System.out.println(i.purchase("p1", 1));
+        System.out.println(i.purchase("p1", 2));
+        System.out.println(i.purchase("p1", 3));
     }
 }
