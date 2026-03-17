21488a757107a95b679454553081a27daff36b8e
poojithaz6 <poojithav183@gmail.com>
2026-03-17 10:49:20 +0530

    problem 4

--- src/w1.java
+++ src/w1.java
@@ -1,36 +1,24 @@
 import java.util.*;
 
-public class w1{
-    static class Entry {
-        String ip;
-        long expiry;
-
-        Entry(String ip, long ttl) {
-            this.ip = ip;
-            this.expiry = System.currentTimeMillis() + ttl;
+public class w1 {
+    static Set<String> grams(String text) {
+        String[] w = text.split(" ");
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < w.length - 2; i++) {
+            set.add(w[i] + " " + w[i + 1] + " " + w[i + 2]);
         }
+        return set;
     }
 
-    Map<String, Entry> map = new HashMap<>();
-    int hits = 0, misses = 0;
-
-    String resolve(String domain) {
-        if (map.containsKey(domain)) {
-            Entry e = map.get(domain);
-            if (System.currentTimeMillis() < e.expiry) {
-                hits++;
-                return e.ip;
-            }
-        }
-        misses++;
-        String ip = "1.1.1." + new Random().nextInt(255);
-        map.put(domain, new Entry(ip, 3000));
-        return ip;
+    static double similarity(String a, String b) {
+        Set<String> s1 = grams(a);
+        Set<String> s2 = grams(b);
+        int match = 0;
+        for (String g : s1) if (s2.contains(g)) match++;
+        return (match * 100.0) / s1.size();
     }
 
     public static void main(String[] args) {
-        w1 d = new w1();
-        System.out.println(d.resolve("google.com"));
-        System.out.println(d.resolve("google.com"));
+        System.out.println(similarity("this is a test text", "this is a test data"));
     }
 }
